d758a2a62f12e2f586b13243ef05f97b501f99f7
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2019-04-19 17:54:57 +0800

    Add code to convert date time

--- batch_jobs/StockTwitAnalytics/src/main/scala/com/descriptiveanalytics/CassandraReader.scala
+++ batch_jobs/StockTwitAnalytics/src/main/scala/com/descriptiveanalytics/CassandraReader.scala
@@ -1,9 +1,14 @@
 package com.descriptiveanalytics
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
+import java.util.Date
+import org.apache.spark.sql.functions.expr
 
 
 object CassandraReader{
+
+
  def main(args: Array[String]): Unit ={
 
 
@@ -26,13 +31,28 @@ object CassandraReader{
    // filter by the stock symbol
    val aaplDF = df.filter(df("symbol") === "AAPL")
 
+   val dateConverterExpr = "new Date(created_at)"
+
    // create intermediate (dummy) columns to store the date and hour from the created_at field
+   val d = new Date(1513714678 * 1000L)
+
+   aaplDF.withColumn("created_at_date", expr(dateConverterExpr)).show()
 
 
    // group by dummy_date (text), dummy_hour, entities_sentiment_basic
-   aaplDF.groupBy("entities_sentiment_basic").count().show()
+   // aaplDF.groupBy("entities_sentiment_basic").count().show()
 
    // get aggregates for counts, likes_total, user_followers
+
+
+   // save the results in the new table in Cassandra
+   /*
+   val dfprev = aaplDF.select("se","hu")
+   dfprev.write.format("org.apache.spark.sql.cassandra")
+     .options(Map("keyspace"->"bigdata","table"->"stock_twits_aggregate"))
+     .mode(SaveMode.Append)
+     .save()
+     */
 
  }
 }
