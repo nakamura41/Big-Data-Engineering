50c35394a590df4ec6bc1c77773c1e9983ff5c21
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2019-04-19 17:26:54 +0800

    Add group by

--- batch_jobs/StockTwitAnalytics/src/main/scala/com/descriptiveanalytics/CassandraReader.scala
+++ batch_jobs/StockTwitAnalytics/src/main/scala/com/descriptiveanalytics/CassandraReader.scala
@@ -19,9 +19,20 @@ object CassandraReader{
      .options(Map( "table" -> "stock_twits", "keyspace" -> "bigdata" ))
      .load()
 
-   //print(df.show(20, false))
-   df.collect.foreach(println)
+   // print(df.show(20, false))
+   // df.collect.foreach(println)
    df.printSchema()
+
+   // filter by the stock symbol
+   val aaplDF = df.filter(df("symbol") === "AAPL")
+
+   // create intermediate (dummy) columns to store the date and hour from the created_at field
+
+
+   // group by dummy_date (text), dummy_hour, entities_sentiment_basic
+   aaplDF.groupBy("entities_sentiment_basic").count().show()
+
+   // get aggregates for counts, likes_total, user_followers
 
  }
 }
