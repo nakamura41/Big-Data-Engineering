c62a44ab36dd1e52475e468ee800be63615f22b4
David Leonardi <davidleonardi418@gmail.com>
2019-04-20 15:10:26 +0800

    StockTwits Kafka Producer.

--- Ingestion-StockTwits-Producer/build.sbt
+++ Ingestion-StockTwits-Producer/build.sbt
@@ -9,4 +9,17 @@ libraryDependencies += "org.apache.kafka" %% "kafka" % "2.1.1"
 
 libraryDependencies += "org.scalaj" %% "scalaj-http" % "2.4.1"
 
-libraryDependencies += "com.typesafe.play" %% "play-json" % "2.6.10"
+// redis
+libraryDependencies += "net.debasishg" %% "redisclient" % "3.9"
+
+// https://mvnrepository.com/artifact/com.google.code.gson/gson
+libraryDependencies += "com.google.code.gson" % "gson" % "2.8.5"
+
+// https://mvnrepository.com/artifact/org.json/json
+libraryDependencies += "org.json" % "json" % "20180813"
+
+// https://mvnrepository.com/artifact/org.json4s/json4s-native
+libraryDependencies += "org.json4s" %% "json4s-native" % "3.6.5"
+
+// https://mvnrepository.com/artifact/org.json4s/json4s-jackson
+libraryDependencies += "org.json4s" %% "json4s-jackson" % "3.6.5"
--- Ingestion-StockTwits-Producer/src/main/scala/com/bigdata/StockTwitsProducer.scala
+++ Ingestion-StockTwits-Producer/src/main/scala/com/bigdata/StockTwitsProducer.scala
@@ -1,14 +1,20 @@
 package com.bigdata
 
 import java.util.Properties
+import java.util.HashMap
 
 import kafka.utils.Logging
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
-import play.api.libs.json._
 import scalaj.http._
+import com.redis._
+import org.json4s._
+import org.json4s.native.JsonMethods._
+import org.json.JSONObject
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+
 
 class StockTwitsProducer() extends Logging {
-
   val props: Properties = createProducerConfig()
   val producer = new KafkaProducer[String, String](props)
 
@@ -25,41 +31,88 @@ class StockTwitsProducer() extends Logging {
     props
   }
 
-  def run(topic: String, stockTicker: String): Unit = {
+  def getDouble(value: JValue): Double = {
+    if (value.values.toString == "None") 0 else value.values.toString.toDouble
+  }
+
+  def getInteger(value: JValue): Int = {
+    if (value.values.toString == "None") 0 else value.values.toString.toInt
+  }
+
+  def getString(value: JValue): String = {
+    value.values.toString
+  }
+
+  def run(topic: String, stockTicker: String, redisHost: String): Unit = {
+    val redis = new RedisClient(redisHost, port = 6379, database = 0)
     val timestamp: Long = System.currentTimeMillis
     val stockUrl: String = s"https://api.stocktwits.com/api/2/streams/symbol/$stockTicker.json"
-    val stockId: String = s"$stockTicker-$timestamp"
 
     val response: HttpResponse[String] = Http(stockUrl)
       .header("Content-Type", "application/json")
       .header("Charset", "UTF-8")
       .option(HttpOptions.readTimeout(10000)).asString
 
-    System.out.println(s"Publish $stockTicker stock tweets: id $stockId")
+    val json = parse(response.body)
+    val jsonResponse = (json \ "response" \ "status").values
+    if (jsonResponse == 200) {
+      val jsonCursor = json \ "cursor"
+      val JInt(jsonSince) = jsonCursor \ "since"
+      val JInt(jsonMax) = jsonCursor \ "max"
+      val since: BigInt = if (jsonSince > jsonMax) jsonMax else jsonSince
+      val max: BigInt = if (jsonSince > jsonMax) jsonSince else jsonMax
 
-    val json: JsObject = Json.parse(response.body).as[JsObject]
-    val jsonTransformer = (__).json.update(
-      __.read[JsObject].map(jsObject => jsObject ++ Json.obj("id" -> stockId))
-    )
+      System.out.println(s"since ID: $since, max ID: $max")
 
-    val newJson = json.transform(jsonTransformer) match {
-      case JsSuccess(jsObject, _) => jsObject
-      case _ => json
+      val messages: List[JsonAST.JValue] = (json \ "messages").children
+      for (message <- messages) {
+        val JInt(messageId) = message \ "id"
+        val stockId: String = s"$stockTicker-$messageId"
+
+        val messsageMap = message
+
+        val jsonMap: HashMap[String, Any] = new HashMap[String, Any]()
+
+        val jsonId: Int = getInteger(message \ "id")
+        val jsonBody: String = getString(message \ "body")
+        val jsonUserFollowers: Int = getInteger(message \ "user" \ "followers")
+        val jsonUserUsername: String = getString(message \ "user" \ "username")
+        val jsonUserName: String = getString(message \ "user" \ "name")
+        val jsonLikesTotal: Int = getInteger(message \ "likes" \ "total")
+        val jsonEntitiesSentimentBasic: String = getString(message \ "entities" \ "sentiment" \ "basic")
+        val jsonSourceId: Int = getInteger(message \ "source" \ "id")
+
+        jsonMap.put("id", jsonId)
+        jsonMap.put("body", jsonBody)
+        jsonMap.put("user_followers", jsonUserFollowers)
+        jsonMap.put("user_username", jsonUserUsername)
+        jsonMap.put("user_name", jsonUserName)
+        jsonMap.put("likes_total", jsonLikesTotal)
+        jsonMap.put("entities_sentiment_basic", jsonEntitiesSentimentBasic)
+        jsonMap.put("source_id", jsonSourceId)
+
+        val gsonMapBuilder = new GsonBuilder
+        val gsonObject = gsonMapBuilder.create
+        val jsonObject = gsonObject.toJson(jsonMap)
+
+        producer.send(new ProducerRecord[String, String](topic, stockId, jsonObject.toString))
+        System.out.println(s"Publish $stockTicker stock tweets: id $stockId")
+      }
+      System.out.println("Message sent successfully")
+
+    } else {
+      logger.error("Failed to get response from StockTwits API")
     }
-    System.out.println(newJson)
-
-    producer.send(new ProducerRecord[String, String](topic, stockId, newJson.toString))
-    System.out.println("Message sent successfully")
     producer.close()
   }
 
 }
 
 object StockTwitsProducer extends App {
-  if (args.length == 2) {
+  if (args.length == 3) {
     val app = new StockTwitsProducer()
-    app.run(args(0), args(1))
+    app.run(args(0), args(1), args(2))
   } else {
-    System.out.println("Enter topic name and stock ticker")
+    System.out.println("Enter topic name, stock ticker, and redis host")
   }
 }
